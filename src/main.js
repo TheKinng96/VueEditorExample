09473d2ce59893eee244d91dc84c3f9760f63af3
TheKinng96 <fengyuan.yap@example.com>
2020-09-07 22:44:15 +0900

    last update before ckeditor

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
+import svgSpriteLoader from "./helpers/svg-sprite-loader";
+import vuetify from "@/plugins/vuetify";
 import router from "./routes";
+
+const __svg__ = {
+  path: "./assets/images/icons/*.svg",
+  name: "assets/images/[hash].sprite.svg",
+};
+svgSpriteLoader(__svg__.filename);
 
 Vue.config.productionTip = false;
 
@@ -9,5 +17,6 @@ Vue.use(VueRouter);
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
